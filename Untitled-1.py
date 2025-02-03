e43d3b1b53cd9610a84aef3973516e352ba316e9
skippka <145149359+skippka@users.noreply.github.com>
2025-02-03 12:44:38 +0200

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-# print("Результат:", 2 + 2, sep="\n", end="")
-# print("Second line \" \t\\ l\nine")
-print(min(2, 3, 4, 5, 6, 7, 8, 9, 10))
-print(max(2, 3, 4, 5, 6, 7, 8, 9, 10))
-print(abs(-100))
-print(round(3.1415926535/ 2))
-
-input("Введите свой возраст: ")
